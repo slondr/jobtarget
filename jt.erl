2847627b66727c83f7f10cd1a0629fe1a8004724
Eric S. Londres <elondres@stevens.edu>
2021-03-09 15:05:15 -0500

    parsing is done

--- jt.erl
+++ jt.erl
@@ -3,7 +3,15 @@
 -export([start/0]).
 
 read(Filename) ->
-    file:read_file(Filename).
+    {ok, Cwd} = file:get_cwd(),
+    AbsoluteFilename = Cwd ++ "/" ++ Filename,
+    file:read_file(AbsoluteFilename).
 
 start() ->
-    io:fwrite("Read mode is ~w~n",[ native_name_encoding()]).
+    io:fwrite("Read mode is ~w~n", [native_name_encoding()]),
+    {ok, ContentString} = read("triangle.txt"),
+    ContentStrings = string:split(ContentString, "\r\n", all),
+    ContentArray = lists:map(fun(X) -> string:lexemes(X, " ") end, ContentStrings),
+    NumericArray = lists:map(fun(X) -> lists:map(fun(Y) -> {I, _} = string:to_integer(Y), I end, X) end, ContentArray),
+    %% NumericArray now contains the triangle as a list of lists where each member list is 1 line of the triangle
+    NumericArray.
